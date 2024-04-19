350bdd2f610713561c0fc1cfac546026b6533e77
Florian <138222545+FlorianMouchet@users.noreply.github.com>
2024-04-19 12:00:26 +0200

    begining

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/app-besson/go-modules
+
+go 1.21.5
---
+++ stringutils.go
@@ -0,0 +1,20 @@
+package stringutils
+
+import (
+	"database/sql"
+	"strings"
+)
+
+// NullIfEmpty renvoie une valeur sql.NullString qui est null si la chaîne d'entrée est null ou ne contient que des espaces blancs,
+// ou la chaîne d'entrée trimée si elle contient une valeur.
+func stringutils(s *string) sql.NullString {
+	if s == nil {
+		return sql.NullString{}
+	}
+	t := strings.TrimSpace(*s)
+
+	if len(t) == 0 {
+		return sql.NullString{}
+	}
+	return sql.NullString{String: t, Valid: true}
+}
